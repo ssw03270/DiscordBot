b4d16de470c0c36b86d7169ebdce733f229cbcc7
SeungWon Seo <ttd8591@gmail.com>
2023-02-04 00:28:39 +0900

    oop

---
+++ bots/user_bot.py
@@ -0,0 +1,65 @@
+import discord
+import asyncio
+
+from utils.user import UserInfo
+
+class UserBot(discord.Client):
+    def __init__(self) -> None:
+        intents = discord.Intents.default()
+        intents.members = True
+        super().__init__(intents=intents)
+        self.user_info = UserInfo()
+
+    async def status_task(self):
+        while True:
+            await self.change_presence(status=discord.Status.idle, activity=discord.Game('status1'))
+            await asyncio.sleep(4)
+            await self.change_presence(status=discord.Status.idle, activity=discord.Game('status2'))
+            await asyncio.sleep(4)
+            await self.change_presence(status=discord.Status.idle, activity=discord.Game('status3'))
+            await asyncio.sleep(4)
+
+    async def on_ready(self):
+        print(f'{self.user.name} is ready')
+        self.loop.create_task(self.status_task())
+
+    async def on_member_join(self, member):
+        name = discord.utils.get(member.guild.members, name=member.name)
+        await member.send(f'{name.mention} 안녕하세요! 경희대학교 소프트웨어융합학과 IIIXR LAB의 공식 채널입니다. 채널에 참여하길 원하신다면, 저에게 구성원 인증을 위한 경희대 이메일을 입력해주세요. (<userid>@khu.ac.kr)')
+
+        # user name을 DB에 추가
+        self.user_info.join(name=member)
+
+    async def on_member_remove(self, member):
+        pass
+
+    async def on_message(self, message):
+        # message의 주인이 bot인 경우
+        if message.author == self.user:
+            return
+
+        permission = self.user_info.get_info(name=message.author, data_type='permission')
+        if permission == 'guest':
+            if "@khu.ac.kr" in message.content:
+                await message.channel.send(f'{message.content}로 전송된 코드를 입력해주세요 (코드를 받지 못했다면 스팸 메일함을 확인해주세요).')
+                await message.channel.send(f'만약 코드 재전송을 원하신다면 \'재전송\'을 입력해주세요.')
+
+                random_code = self.user_info.send_email(message.content, 'IIIXR LAB Discord Email Verification', 'IIIXR LAB')
+                self.user_info.set_info(name=message.author, data_type='random_code', data_content=random_code)
+                self.user_info.set_info(name=message.author, data_type='email', data_content=message.content)
+
+            if "재전송" == message.content:
+                email = self.user_info.get_info(name=message.author, data_type='email')
+                random_code = self.user_info.send_email(email, 'IIIXR LAB Discord Email Verification', 'IIIXR LAB')
+                self.user_info.set_info(name=message.author, data_type='random_code', data_content=random_code)
+
+            if message.content == self.user_info.get_info(name=message.author, data_type='random_code'):
+                await message.channel.send(f'Guest 역할을 지급하였습니다. 환영합니다.')
+                self.user_info.set_info(name=message.author, data_type='permission', data_content='member')
+
+                guild = discord.utils.get(self.guilds, name='Test')
+                role = discord.utils.get(guild.roles, name='Guest')
+                member = discord.utils.get(guild.members, name=message.author.name,
+                                           discriminator=message.author.discriminator)
+
+                await member.add_roles(role, reason="디스코드봇 자동부여")
---
+++ utils/user.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import smtplib
+import string
+import random
+from email.message import EmailMessage
+
+class UserInfo():
+    def __init__(self):
+        self.DB = self.set_DB()
+
+    def set_DB(self):
+        try:
+            self.DB = pd.read_csv('../db.csv', low_memory=False)
+        except:
+            self.DB = pd.DataFrame(columns=['name', 'email', 'random_code', 'permission'])
+            self.DB.to_csv('db.csv')
+
+        return self.DB
+
+    def join(self, name):
+        temp = pd.DataFrame({'name': [name], 'email': [''], 'random_code': [''], 'permission': ['guest']})
+        self.DB = pd.concat([self.DB, temp])
+        print(f'{name} join')
+
+    def set_info(self, name, data_type, data_content):
+        self.DB.loc[self.DB['name'] == name, data_type] = data_content
+
+    def get_info(self, name, data_type):
+        length = len(self.DB.loc[self.DB['name'] == name, data_type].values)
+        if length == 0:
+            return ''
+        else:
+            return self.DB.loc[self.DB['name'] == name, data_type].values[0]
+
+    def send_email(self, target, title, name):
+        smtp_gmail = smtplib.SMTP('smtp.gmail.com', 587)
+
+        # 서버 연결을 설정하는 단계
+        smtp_gmail.ehlo()
+
+        # 연결을 암호화
+        smtp_gmail.starttls()
+
+        # 로그인
+        smtp_gmail.login('ssw03270@khu.ac.kr', 'tjtmddnjs01')
+
+        msg = EmailMessage()
+
+        # 제목 입력
+        msg['Subject'] = title
+        # 내용 입력
+        code = self.make_random_code()
+        msg.set_content('IIIXR LAB에 오신 것을 환영합니다! \n여기에 귀하의 액세스 코드가 있습니다: ' + code + '\n이 코드를 누구와도 공유하지 마십시오.')
+        # 보내는 사람
+        msg['From'] = name
+        # 받는 사람
+        msg['To'] = target
+
+        smtp_gmail.send_message(msg)
+        return code
+
+    def make_random_code(self):
+        _LENGTH = 6  # 몇자리?
+        string_pool = string.digits  # "0123456789"
+        result = ""  # 결과 값
+        for i in range(_LENGTH):
+            # 랜덤한 하나의 숫자를 뽑아서, 문자열 결합을 한다.
+            result += random.choice(string_pool)
+
+        return result
